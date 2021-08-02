20cd52b8172eb465f72b4d53c557ff6356a1f601
Lafen Lesley <lesleytech6@gmail.com>
2021-08-02 16:57:11 +0300

    update img path

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
         {home && (
           <>
             <img
-              src='blog/images/profile.jpg'
+              src='images/profile.jpg'
               className={utilStyles.borderCircle}
               height={144}
               width={144}
